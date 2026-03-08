fc21a2496bf5abbb5bf3fd94d47d11ee71641c90
CreativeMitra <upkartdailyfinds@gmail.com>
2026-03-08 13:40:31 +0530

    Add ESLint configuration to ignore during builds

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
